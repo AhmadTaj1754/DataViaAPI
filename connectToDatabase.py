06c8f5921c7fd44cdc40fec8b75b817801733c8c
Ahmad Tajgardoun <ahmadtaj@Ahmads-MacBook-Pro.local>
2019-11-29 12:18:16 -0500

    added a graph generator

--- connectToDatabase.py
+++ connectToDatabase.py
@@ -1,10 +1,9 @@
-
-
 import requests
 import json
 import csv
 import mysql.connector
-
+import plotly.graph_objects as go
+import numpy as np
 
 #Obtain "Chained Consumer Price Index for All Urban Consumers: All Items in U.S.
 #City Average " data from U.S. Bureau of Labor Statistics via public API, in
@@ -27,10 +26,7 @@ with open('file.cvs', 'w') as csvfile:
             if series['seriesID']=="SUUR0000SA0":
                 seriesName = "Chained Consumer Price Index for All Urban Consumers: All Items in U.S. City Average "
             else:
-                seriesName = "Not Available"
-            for footnote in item['footnotes']:
-                if footnote:
-                    footnotes = footnotes + footnote['text']
+                seriesName = "Not Available
             datawriter.writerow([seriesId,seriesName,year,period,periodName, value,footnotes[0:2]])
 
 #connect to MySQL database and insert values obtianed with BLS API into
@@ -42,6 +38,8 @@ mydb = mysql.connector.connect(
   database="BLS",
   buffered=True,
 )
+
+#Insert data into database utilizing csv file 
 mycursor = mydb.cursor()
 with open('file.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -49,7 +47,40 @@ with open('file.csv') as csvfile:
         sqlstatement="INSERT INTO Chained_ConsumerPrice_US_City_Average(SeriesID,SeriesName, year,period, PeriodName, DataPoint, Footnotes) Values({0},{1},{2},{3},{4},{5},{6})".format(repr(row[0]),repr(row[1]),repr(row[2]),repr(row[3]),repr(row[4]),repr(row[5]),repr(row[6]))
         mycursor.execute(sqlstatement)
 mydb.commit()
-mycursor.execute("SHOW TABLES")
+
+
+#Get data and create a quick chart
+rsqlstatement ="SELECT DataPoint From Chained_ConsumerPrice_US_City_Average Where year=2000 Order By year, Period"
+mycursor.execute(rsqlstatement)
+
+sorteddata=[]
+for DataPoint in mycursor:
+    sorteddata+=DataPoint
+
+#draw a quick graph for data
+fig = go.Figure(data=go.Scatter(x=['January', 'February', 'March', 'April', 'May',
+'June', 'July', 'August', 'September', 'October', 'November', 'December',], y=sorteddata),
+layout_title_text="year 2000 - Chained Consumer Price Index for All Urban\n"+
+                   "Consumers: All Items in U.S. City Average"
+)
+fig.show()
+
+mycursor.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
